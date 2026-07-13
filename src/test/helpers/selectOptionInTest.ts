5138385d9dd754d63400c20b053db7192c1b0b39
ismail simsek <ismailsimsek09@gmail.com>
2026-07-13 14:26:06 +0200

    remove unused test files

--- src/test/helpers/selectOptionInTest.ts
+++
@@ -1,7 +0,0 @@
-import { waitFor } from '@testing-library/react';
-import { select } from 'react-select-event';
-
-export const selectOptionInTest = async (
-  input: HTMLElement,
-  optionOrOptions: string | RegExp | Array<string | RegExp>
-) => await waitFor(() => select(input, optionOrOptions, { container: document.body }));
